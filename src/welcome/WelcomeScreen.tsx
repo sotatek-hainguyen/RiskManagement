9cc8afd8bfb2957c950c4a46f9e70342a65c7364
HaiNguyen-Sotatek <hai.nguyen@sotatek.com>
2022-07-13 16:53:15 +0700

    remove styled component

--- src/welcome/WelcomeScreen.tsx
+++ src/welcome/WelcomeScreen.tsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 import Logo from '../../assets/images/logo.svg';
 import {RootStackParamList} from '../main/Navigation';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {mainRoutes} from '../main/mainRoutes';
 import {Button} from 'react-native-elements';
-import styled from 'styled-components';
 
 const WelcomeScreen = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
 
-  const Wrapper = styled.section`
-    padding: 4em;
-    margin-top: 10px;
-    background: papayawhip;
-  `;
   return (
     <View style={styles.container}>
       <Text style={styles.title}>WelcomeScreen</Text>
@@ -33,9 +21,6 @@ const WelcomeScreen = () => {
         title="Click to Auth"
         onPress={() => navigation.navigate(mainRoutes.Auth)}
       />
-      <Wrapper>
-        <Title>Hello World!</Title>
-      </Wrapper>
     </View>
   );
 };
